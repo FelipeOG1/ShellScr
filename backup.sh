d9abc2e365a9d95d1ecb181700b52c8b9f3000f3
felipe <feliog1234@gmail.com>
2025-05-31 20:15:13 -0600

    final commit

--- backup.sh
+++ backup.sh
@@ -16,19 +16,40 @@ fi
 targetDirectory=$1
 destinationDirectory=$2
 
-echo "$destinationDirectory"
-
 currentTS=$(date +%s)
 
 backupFileName="backup-$currentTS.tar.gz"
 
+
 origAbsPath=$(pwd)
 
 
-cd $destinationDirectory  
+cd "$destinationDirectory"
+
 destDirAbsPath=$(pwd)
-echo $destDirAbsPath
+
+cd "$targetDirectory"
+
+yesterdayTS=$((currentTS - 24 * 60 * 60))
+  
 
 
+declare -a toBackup
+
+
+for file in * 
+do
+  currentFileDate=$(date -r "$file" +%s)
+
+  if [[ "$currentFileDate" -gt "$yesterdayTS" ]] 
+  then
+    toBackup+=($file)
+    
+   fi
+done
+  
+tar -czvf "$backupFileName" "${toBackup[@]}"
+
+mv "$backupFileName" "$destDirAbsPath"
 
 
